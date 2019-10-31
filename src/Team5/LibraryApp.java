189b7fdb228655d19fb29decab2074eb5bed46ca
matthewCLynn <matthewchristopherlynn@yahoo.com>
2019-10-31 14:08:35 -0400

    added keyword search and updated formatting, added test data

--- src/Team5/LibraryApp.java
+++ src/Team5/LibraryApp.java
@@ -11,6 +11,10 @@ public class LibraryApp {
 		// createList
 		int counter = 0;
 		ArrayList<Book> bookList = new ArrayList<>();
+		bookList.add(new Book("It follows", "me", true, "12/12/12"));
+		bookList.add(new Book("it", "me", true, "12/12/12"));
+		bookList.add(new Book("steve", "you", false, "12/12/12"));
+		bookList.add(new Book("It is all good in the hood", "you", false, "/12/12/12"));
 		String cont = "n";
 		// set arraylist = BookHelper.readBookList()
 
@@ -18,70 +22,96 @@ public class LibraryApp {
 
 		do {
 
-		// menu
+			// menu
 			System.out.println(
-					"1. List all books\n2. Search by author\n3. Search by title or title keyword\n4. Check out a book\n5. Return a book\n6. Donate a book\n7. Exit");
+					"\n1. List all books\n2. Search by author\n3. Search by title or title keyword\n4. Check out a book\n5. Return a book\n6. Donate a book\n7. Exit");
 			int menuChoice = Validator.getInt(scan, "\nWhat would you like to do?", 1, 7);
 
-		// List of books title author status
-		// search by author
-		// search by title keyword
-		// check out a book (reiterate book list)
-		// -if not checked out, check out for 2 wks
-		// - if checked out already, inform user and ask if they would like to be put on
-		// hold list
-		// return a book
-		// Donate a book
-		// Exit
+			// List of books title author status
+			// search by author
+			// search by title keyword
+			// check out a book (reiterate book list)
+			// -if not checked out, check out for 2 wks
+			// - if checked out already, inform user and ask if they would like to be put on
+			// hold list
+			// return a book
+			// Donate a book
+			// Exit
 
-		switch (menuChoice)	{
+			switch (menuChoice) {
 
-		case 1:	// List of books title author status
-				System.out.printf("%-30s  %-30s %-15s", "Title", "Author", "Status");
+			case 1: // List of books title author status
+				System.out.printf("%-30s  %-30s %-15s", "   Title", "Author", "Status");
+				System.out.println();
 				for (int i = 0; i < bookList.size(); i++) {
-					System.out.printf("%-30s  %-30s %-15s", (i + 1) + bookList.get(i).getTitle(),
+					System.out.printf("\n%-30s  %-30s %-15s", (i + 1) + ". " + bookList.get(i).getTitle(),
 							bookList.get(i).getAuthor(), bookList.get(i).getStatus());
 
 				}
-			
-			break;
-			
-		case 2:	// search by author
+				System.out.println();
+				break;
+
+			case 2: // search by author
 				System.out.println("Please enter the name of the author you would like to search: ");
 				String bookAuthor = scan.nextLine();
 				counter = 0;
 				for (int i = 0; i < bookList.size(); i++) {
-					if (bookAuthor.equals(bookList.get(i).getAuthor())) {
-						System.out.printf("%-30s %-15s", counter++ + bookList.get(i).getTitle(),
+					if (bookAuthor.equalsIgnoreCase(bookList.get(i).getAuthor())) {
+						System.out.printf("\n%-30s %-15s", counter++ + bookList.get(i).getTitle(),
 								bookList.get(i).getStatus());
 					}
 				}
-			
-			break;
-			
-		case 3:	// search by title keyword
-			
-			break;
-			
-		case 4:	// check out a book (reiterate book list)
-			
-			break;
-			
-		case 5:	// return a book
-			
-			break;
-			
-		case 6:	// Donate a book
-		
-			break;
-		case 7:	// Exit
+
+				break;
+
+			case 3: // search by title keyword
+
+				System.out.print("Please enter a title or title keyword for the book you are looking for: ");
+				String keyWord = scan.nextLine();
+				counter = 0;
+				for (int i = 0; i < bookList.size(); i++) {
+					// check if exact title match and display before other books
+					if (keyWord.equalsIgnoreCase(bookList.get(i).getTitle())) {
+						System.out.printf("\n%-30s %-30s %-15s", ++counter + ". " + bookList.get(i).getTitle(),
+								bookList.get(i).getAuthor(), bookList.get(i).getStatus());
+					}
+
+				}
+				for (int i = 0; i < bookList.size(); i++) {
+					// check for keyword and display matches
+					String[] keyWords = bookList.get(i).getTitle().split(" ");
+					for (int j = 0; j < keyWords.length; j++) {
+						//confirms that it isn't found twice (not the whole title)
+						if ((keyWord.equalsIgnoreCase(keyWords[j]))
+								&& (!keyWord.equalsIgnoreCase(bookList.get(i).getTitle()))) {
+							
+							System.out.printf("\n%-30s %-30s %-15s", ++counter + ". " + bookList.get(i).getTitle(),
+									bookList.get(i).getAuthor(), bookList.get(i).getStatus());
+						}
+					}
+
+				}
+
+				break;
+
+			case 4: // check out a book (reiterate book list)
+
+				break;
+
+			case 5: // return a book
+
+				break;
+
+			case 6: // Donate a book
+
+				break;
+			case 7: // Exit
 				cont = Validator.getStringMatchingRegex(scan, "\nAre you sure you want to leave the library? (y/n): ",
 						"[YyNn]");
-			
-			
-			break;
-		} 
-		}while(cont.equalsIgnoreCase("n"));
+
+				break;
+			}
+		} while (cont.equalsIgnoreCase("n"));
 
 		//
 		System.out.println("\nEnjoy your reading! Come back in 14 Days!");
