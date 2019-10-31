b3148ceff6c7a8c470360409c6bafd12b48296f2
kbaskin <kathrynbaskin@gmail.com>
2019-10-31 10:26:22 -0400

    initial commit

---
+++ src/Team5/Book.java
@@ -0,0 +1,59 @@
+package Team5;
+
+public class Book {
+
+	private String title;
+	private String author;
+	private boolean status;
+	private String dueDate;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String title, String author, boolean status, String dueDate) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.status = status;
+		this.dueDate = dueDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%-30s  %-20s %-15b %-10s", title, author, status, dueDate);
+	}
+
+}
